1fa2537b068feb3846bdf49ae5af956addffdda0
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-01-11 21:31:54 -0300

    controller products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    private function index()
+    {
+        $products = Product::all();
+
+        return view('products.index', compact('products'));
+    }
 }
