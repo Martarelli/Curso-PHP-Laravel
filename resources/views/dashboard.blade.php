b492b99eace491822262351234e755b01f8cd87d
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-01-13 21:49:13 -0300

    navbar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+    @include('components.navbar')
+
     <div class="min-vh-100 d-flex justify-content-center align-items-center flex-column" style="margin-top: 80px">
 
         <h1>Hello World!</h1>
