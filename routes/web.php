3b96373d316b0e56251adae09d68e232433a1f71
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-01-16 21:36:18 -0300

    rota logout

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/login', [UserController::class, 'login']) -> name('login');
 Route::get('/register', [UserController::class, 'register']);
 Route::post('/signin', [UserController::class, 'signin']);
 Route::post('/signup', [UserController::class, 'signup']);
+Route::get('/logout', [UserController::class, 'logout']);
 
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::post('/posts/store', [PostController::class, 'store']);
