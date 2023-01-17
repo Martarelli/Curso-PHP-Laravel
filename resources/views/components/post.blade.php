0d7123900fb9a26552f71abfc2eb99ab9b49dc17
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-01-17 20:01:55 -0300

    acessando nome do dono do post

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,7 +1,7 @@
 <div class="card mb-3" style="width: 600px; max-width: 100%">
 
     <div class="card-header bg-white border-0">
-        <span class="fw-bold">{{$post->user_id}}</span>
+        <span class="fw-bold">{{$post->user->name}}</span>
     </div>
 
     <img src="{{$post->image}}" alt="image">
