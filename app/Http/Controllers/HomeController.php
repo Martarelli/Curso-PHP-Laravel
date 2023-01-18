14ad229f3ea97334a1fe990f2ce2c5094811e68f
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-01-18 19:51:45 -0300

    exemplos logs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,20 @@ class HomeController extends Controller
 {
     public function index ()
     {
-        logger()->debug('Aqui é um log!');
+        $num1 = 5;
+        $num2 = 10;
+        logger()->debug('Aqui é um log antes da soma!');
+
+        $soma = $num1 + $num2;
+        logger()->info('A soma ficou:', compact('soma'));
+        $subt = $num1 - $num2;
+        logger()->warning('A subtração ficou:', compact('num1','num2','subt'));
+
+        if ($subt < 0)
+        {
+            logger()->error('Valor negativo:', compact('subt'));
+        }
+
 
         return 'Hello world!';
     }
